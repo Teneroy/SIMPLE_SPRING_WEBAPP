6d65e46a18d718228e65ae304f9776e2822484dd
Teneroy <alexchernishof@gmail.com>
2020-05-12 19:16:13 +0300

    Added the ability to send files

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -5,20 +5,28 @@ import org.example.domain.Role;
 import org.example.domain.User;
 import org.example.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Model model) {
@@ -27,6 +35,7 @@ public class MainController {
 
     @GetMapping("/error")
     public String error(Model model) {
+
         return "error";
     }
 
@@ -40,11 +49,34 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Model model) {
+    public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Model model, @RequestParam("file") MultipartFile file) throws IOException {
         if(text.isEmpty() || tag.isEmpty()) {
             return main(user, "", model);
         }
         Message message = new Message(text, tag, user);
+        if(file != null) {
+            File uploadDir = new File(uploadPath);
+            boolean mkdirResult = true;
+
+            if(!uploadDir.exists()) {
+                mkdirResult = uploadDir.mkdir();
+            }
+
+            if(mkdirResult)
+            {
+                String uuidFile = UUID.randomUUID().toString();
+                String resultFilename = uuidFile + "." + file.getOriginalFilename();
+                boolean fileUploaded = true;
+                try {
+                    file.transferTo(new File(uploadPath + "/" + resultFilename));
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    fileUploaded = false;
+                }
+                if (fileUploaded)
+                    message.setFilename(resultFilename);
+            }
+        }
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
         model.addAttribute("messages", messages);
