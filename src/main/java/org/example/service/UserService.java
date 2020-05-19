0b8ea0c69b2fa05461d0047aa26528f9b8937fd5
Teneroy <alexchernishof@gmail.com>
2020-05-19 21:22:32 +0300

    Fixed profile encryption bug

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -119,7 +119,7 @@ public class UserService implements UserDetailsService {
         }
 
         if(password != null && !password.isEmpty()) {
-            user.setPassword(password);
+            user.setPassword(passwordEncoder.encode(password));
         }
 
         userRepo.save(user);
