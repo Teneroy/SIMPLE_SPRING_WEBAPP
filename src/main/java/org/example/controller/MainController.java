839a390d6efb75cf440a98d06bf0f3ce12586c46
Teneroy <alexchernishof@gmail.com>
2020-05-21 00:11:34 +0300

    Added the ability to read messages

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -14,12 +14,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -78,5 +80,20 @@ public class MainController {
         model.addAttribute("admin", ControllerUtils.getRole(user));
 
         return "main";
+    }
+
+    @GetMapping("/user-messages/{user}")
+    public String userMessages(
+        @AuthenticationPrincipal User currentUser,
+        @PathVariable User user,
+        Model model
+    ) {
+        Set<Message> messages = user.getMessages();
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("isCurrentUser", currentUser.equals(user));
+        model.addAttribute("admin", ControllerUtils.getRole(user));
+
+        return "userMessages";
     }
 }
